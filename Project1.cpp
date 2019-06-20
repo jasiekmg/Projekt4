1273da86f31ec0f1502c24af8925dc00738f0bb7
iqorex <51821001+iqorex@users.noreply.github.com>
2019-06-20 18:24:54 +0200

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,36 @@
+﻿// Project1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include "Paddle.h"
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	Paddle paddle(400, 550);
+	RenderWindow window{ VideoMode {800,600}, "dzwig" };
+	window.setFramerateLimit(60);
+	Event event;
+	while (true)
+	{
+		window.clear(Color::Black);
+		window.pollEvent(event);
+
+		if (event.type == Event::Closed)
+		{
+			window.close();
+			break;
+		}
+		paddle.update();
+		window.draw(paddle);
+		window.display();
+
+	}
+
+
+	return 0;
+}
